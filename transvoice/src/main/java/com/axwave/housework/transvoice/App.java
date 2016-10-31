a9477fd5bf25b9ff2664f9b376db178a199cc444
Karol Kulesza <196012@student.pwr.wroc.pl>
2016-10-31 18:30:24 +0100

    Init

---
+++ transvoice/src/main/java/com/axwave/housework/transvoice/App.java
@@ -0,0 +1,13 @@
+package com.axwave.housework.transvoice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
